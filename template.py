3498d5ddd8871a7b1dbec906456a4d1261749081
minuk00 <apolo0430@naver.com>
2020-06-21 11:23:32 +0900

    Add File

---
+++ template.py
@@ -0,0 +1,13 @@
+html = """
+<html>
+        <body>
+                <form action="">
+                                first number = <input type = "number" name = "first_num"> <br><br>
+                                second number = <input type = "number" name = "second_num">
+                        <input type="submit">
+                </form>
+                sum = %(sum)d <br><br>
+                mul = %(mul)d
+        </body>
+</html>
+"""
