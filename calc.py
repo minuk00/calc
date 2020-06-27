7bc4c3b3722b2a4fee1211b3f63ced550e184e4d
minuk00 <66010060+minuk00@users.noreply.github.com>
2020-06-27 16:54:16 +0900

    Add insdigit() #3

--- calc.py
+++ calc.py
@@ -7,6 +7,7 @@ def application(environ, start_response):
         second_num = d.get('second_num', [''])[0]
         sum, mul = 0, 0
         if '' not in [first_num, second_num]:
+        if first_num.insdigit() and second_num.insdigit():
                 first_num, second_num = int(first_num), int(second_num)
                 sum = first_num + second_num
                 mul = first_num * second_num
